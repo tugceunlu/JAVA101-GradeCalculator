167eaf06ce87dc93619eb8802e0d2cd36e851f14
Tuğçe ünlü <72610188+tugceunlu@users.noreply.github.com>
2023-01-27 23:59:03 +0300

    Add files via upload

---
+++ NotOrtalamasıHesaplama.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class NotOrtalamasiHesaplama {
+    public static void main(String[] args) {
+        int music, math,turkish, history,phys, chem;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Grade/Music:");
+        music = input.nextInt();
+        System.out.println(music);
+        System.out.print("Grade/Math:");
+        math = input.nextInt();
+        System.out.println(math);
+        System.out.print("Grade/History:");
+        history = input.nextInt();
+        System.out.println(history);
+        System.out.print("Grade/Phys:");
+        phys = input.nextInt();
+        System.out.println(phys);
+        System.out.print("Grade/Chem:");
+        chem = input.nextInt();
+        System.out.println(chem);
+        System.out.print("Grade/Turkish:");
+        turkish = input.nextInt();
+        System.out.println(turkish);
+
+        int average;
+        average = (math + music + history + phys + turkish + chem)/6;
+        System.out.print("Average Grade: ");
+        System.out.print(average);
+
+
+
+    }
+
+}
+
